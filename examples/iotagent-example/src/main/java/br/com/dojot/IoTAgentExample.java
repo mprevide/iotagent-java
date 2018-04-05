0d30e277255de77ef236b1a918882c32ee1ffc63
Tiago Primini <tiagopri@cpqd.com.br>
2018-04-05 13:28:24 -0300

    Include iota example

---
+++ examples/iotagent-example/src/main/java/br/com/dojot/IoTAgentExample.java
@@ -0,0 +1,41 @@
+package br.com.dojot;
+
+import br.com.dojot.kafka.Manager;
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
+
+public class IoTAgentExample {
+
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(IoTAgentExample.class);
+        Agent agent = new Agent();
+
+        Manager manager = new Manager();
+
+        manager.addCallback("create", agent::on_create);
+        manager.addCallback("update", agent::on_update);
+        manager.addCallback("delete", agent::on_delete);
+
+        while (true) {
+            logger.info("Running IoTAgent Example");
+
+            logger.info(manager.listDevices("admin").toString());
+
+            JSONObject eventData = new JSONObject();
+
+            eventData.put("param_1", "valor_1");
+            eventData.put("param_2", "valor_2");
+            eventData.put("param_3", "valor_3");
+
+            logger.info(eventData.toString());
+
+            manager.updateAttrs("8743ba", "admin", eventData, null);
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException exception) {
+                logger.error("Exception: " + exception.toString());
+            }
+        }
+    }
+}
